3bd4ee9c0984f046d924d61f2840afe98b2e16c8
guillaume.laurent <guillaume.laurent@sightcall.com>
2025-09-23 15:23:21 +0200

    Published SightCallSDK version 42.42.44

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "SightCallSDK",
-            url: "https://sightcall-ios-cocoapods.s3.amazonaws.com/SightCallSDK/42.42.43/SightCallSDK.xcframework.zip",
-            checksum: "31755dc2b103c650a1b542bc5aa4efce068e778f60ac893662865e0c7d457c11"
+            url: "https://sightcall-ios-cocoapods.s3.amazonaws.com/SightCallSDK/42.42.44/SightCallSDK.xcframework.zip",
+            checksum: "238157324cdd569bf45f971fd5aaac2cf9152d628a7bcf14d5b9c182397f6f95"
         )
     ]
 )
